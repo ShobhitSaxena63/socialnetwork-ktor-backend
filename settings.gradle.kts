fbab7801833949bff5563418fe4c846f45d70d93
shobhit-solulab <shobhit.solulab@gmail.com>
2023-11-22 13:00:16 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.shobhit63.socialnetwork"
---
+++ src/main/kotlin/com/shobhit63/Application.kt
@@ -0,0 +1,16 @@
+package com.shobhit63
+
+import com.shobhit63.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureHTTP()
+    configureSecurity()
+    configureRouting()
+}
